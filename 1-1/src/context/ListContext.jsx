55645dd90ab1beb7444454629f35d1786993356e
hyoju <gywn9708@gmail.com>
2022-11-27 23:56:27 +0900

    Feat: issueList, detail 구현

--- 1-1/src/context/ListContext.jsx
+++ 1-1/src/context/ListContext.jsx
@@ -1,7 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
-import { createContext } from "react";
+import React, { useState, createContext, useCallback, useEffect } from "react";
+import { getIssue, getIssueList } from "../api";
 
-export const ListContext = createContext({ children });
-export const ListContextProvider = () => {
-  return <ListContextProvider value={value}>{children}</ListContextProvider>;
+export const ListContext = createContext({
+  issueList: [],
+  issue: [],
+});
+
+export const ListContextProvider = ({ children }) => {
+  const [issueList, setIssueList] = useState([]);
+  const [issue, setIssue] = useState([]);
+
+  const handleIssueList = useCallback(() => {
+    getIssueList()
+      .then((res) => {
+        console.log(res);
+        setIssueList(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  useEffect(() => {
+    handleIssueList();
+  }, [handleIssueList]);
+
+  const handleIssue = useCallback((number) => {
+    getIssue(number)
+      .then((res) => {
+        console.log("res", res);
+        setIssue(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <ListContext.Provider value={{ issueList, issue, handleIssue }}>
+      {children}
+    </ListContext.Provider>
+  );
 };
